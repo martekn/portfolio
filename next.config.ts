26b8cf4315197bf8f662bb1550ec5fda419055b8
martekn <marte.mk@hotmail.com>
2025-05-05 21:34:54 +0200

    Setup nextjs static config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+  output: "export",
 };
 
 export default nextConfig;
