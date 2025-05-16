09004a2cbbb2e5b101f5a1d02be98e532b19d042
martekn <marte.mk@hotmail.com>
2025-05-16 20:05:39 +0200

    Add site footer

---
+++ src/components/ui/site-footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "./container";
+
+const SiteFooter = () => {
+  return (
+    <div className="bg-primary-900/35 text-sm text-center py-2 text-primary-100/80">
+      <Container variant={"fullBleed"}>©2025 martekn</Container>
+    </div>
+  );
+};
+
+export default SiteFooter;
